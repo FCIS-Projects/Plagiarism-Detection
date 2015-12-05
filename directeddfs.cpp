8a7cd3b9ec0817cb9cc96e94186aaad1714e69aa
Sara <saraadel.fcis2012@gmail.com>
2015-12-05 17:13:05 +0200

    overload the constructor of class DirectedDFS

--- directeddfs.cpp
+++ directeddfs.cpp
@@ -1,6 +1,12 @@
 #include "directeddfs.h"
 #include "directedgraph.h"
 
+DirectedDFS::DirectedDFS(DirectedGraph *digraph)
+{
+    marked = new bool[digraph->nodes_number];
+    this->digraph = digraph;
+}
+
 DirectedDFS::DirectedDFS(DirectedGraph *digraph, int node)
 {
     marked = new bool[digraph->nodes_number];
