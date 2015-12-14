da41acbc2ec300a909375c19cab5e43ac0f1d542
Baron Leonardo <mohamedayman.fcis@zoho.com>
2015-12-14 18:54:23 +0200

    SB:

--- directedgraph.cpp
+++ directedgraph.cpp
@@ -16,7 +16,7 @@ DirectedGraph::DirectedGraph(int nodes_number)
     for (int iii = 0; iii < nodes_number; ++iii)
     {
 //        nodes_list->insert(iii, new QVector<int>);
-        nodes_list->append(new Node);
+        nodes_list->append( (*new Node) );
     }
 }
 
@@ -34,8 +34,8 @@ void DirectedGraph::add_edge(int head, int tail)
     check_node_validity(head);
     check_node_validity(tail);
 
-    if( nodes_number == 0 && nodes_list->length() != NULL )
-        nodes_list->append(new Node);
+    if( nodes_number == 0 && nodes_list->length() != 0 )
+        nodes_list->append(*(new Node));
 //        nodes_list->insert(head, new QVector<int>);
 
     (*nodes_list)[head].connections.append(tail);
