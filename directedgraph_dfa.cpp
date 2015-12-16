49c3ae84c69951a58e728720805c37b2f7653f14
Baron Leonardo <mohamedayman.fcis@zoho.com>
2015-12-16 12:50:23 +0200

    reconstruct 'directedgraph_dfa' class

--- directedgraph_dfa.cpp
+++ directedgraph_dfa.cpp
@@ -1,8 +1,28 @@
 #include "directedgraph_dfa.h"
 
-DirectedGraph_DFA::DirectedGraph_DFA(MAP* nfa) : DirectedGraph()
-{
+using namespace dfa_digraph;
 
+DirectedGraph_DFA::DirectedGraph_DFA() : DirectedGraph()
+{
+    nodes_list = new DFA_MAP;
+}
+
+int DirectedGraph_DFA::create_node()
+{
+    // Create new node and append it to the array
+    nodes_list->append( (*new Node) );
+
+    // Change the 'index' variable into the current index
+    int last_node_index = nodes_list->length() - 1;
+    (*nodes_list)[last_node_index].index = last_node_index;
+
+    // Update the number of nodes
+    nodes_number++;
+
+
+    nodes_list->last().is_finial_state = false;
+
+    return last_node_index;
 }
 
 void DirectedGraph_DFA::add_edge( int head, int tail )
@@ -13,15 +33,17 @@ void DirectedGraph_DFA::add_edge( int head, int tail )
 //    if( dfa->size() == 0 && (*dfa)[tail] != NULL )
 //    QVector<int> *vertex = dfa->operator [](head);
 
-    Node *tail_node = new Node;
-    tail_node->next_state.append(tail);
 
-    dfa->insert(head, tail_node);
+//    tail_node->next_state.append(tail);
+//    Node *tail_node = new Node;
+//    dfa->insert(-1, tail_node);
+
+    (*nodes_list)[head].connections.append(tail);
 }
 
-DFA_MAP* DirectedGraph_DFA::get_dfa()
+DFA_MAP* DirectedGraph_DFA::get_nodes_list()
 {
-    return dfa;
+    return nodes_list;
 }
 
 DirectedGraph_DFA::~DirectedGraph_DFA()
--- directedgraph_dfa.h
+++ directedgraph_dfa.h
@@ -1,32 +1,38 @@
 #ifndef DIRECTEDGRAPH_DFA_H
 #define DIRECTEDGRAPH_DFA_H
 #include "directedgraph.h"
-#include <QMap>
+#include <QList>
 
-struct Node
+namespace dfa_digraph
 {
-    bool is_finial_state;
-    QVector<int> next_state;
-};
-
-class DirectedGraph_DFA : private DirectedGraph
-{
-public:
-    DirectedGraph_DFA(MAP* nfa);
-    ~DirectedGraph_DFA();
-
-
-    #define DFA_MAP QMap< int, Node* >
-
-    void build_reachable_states(int node);
-    void build_reachable_states(QVector<int>* nodes);
-    void add_edge( int head, int tail );
-
-private:
-    DFA_MAP* dfa;
-
-public:
-    DFA_MAP* get_dfa();
-};
-
+    struct Node
+    {
+        int index;
+        QList<int> connections;
+        bool is_finial_state;
+        QList<int> match_transitions;
+    };
+    
+    class DirectedGraph_DFA : public digraph::DirectedGraph
+    {
+    public:
+        DirectedGraph_DFA();
+        ~DirectedGraph_DFA();
+    
+    
+        #define DFA_MAP QList< dfa_digraph::Node >
+    
+//        void build_reachable_states(int node);
+//        void build_reachable_states(QVector<int>* nodes);
+        int create_node();
+        void add_edge( int head, int tail );
+    
+    private:
+        DFA_MAP* nodes_list;
+    
+    public:
+        DFA_MAP* get_nodes_list();
+    };
+}
+    
 #endif // DIRECTEDGRAPH_DFA_H
