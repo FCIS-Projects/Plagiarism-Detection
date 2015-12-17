bb713e45e21914acffe91612bc23e7baf3101b6b
Baron Leonardo <mohamedayman.fcis@zoho.com>
2015-12-17 12:57:13 +0200

    if this node (which has no connections) created before mark it as true

--- directeddfs_dfa.cpp
+++ directeddfs_dfa.cpp
@@ -5,16 +5,19 @@ using namespace digraph;
 DirectedDFS_DFA::DirectedDFS_DFA(DirectedGraph *digraph) : DirectedDFS( digraph )
 {
     reachable_states = new QList<int>;
+    created_nodes = new QVector<bool>(digraph->get_nodes_list()->length());
 }
 
 DirectedDFS_DFA::DirectedDFS_DFA(DirectedGraph *digraph, int node) : DirectedDFS( digraph, node )
 {
     reachable_states = new QList<int>;
+    created_nodes = new QVector<bool>(digraph->get_nodes_list()->length());
 }
 
 DirectedDFS_DFA::DirectedDFS_DFA(DirectedGraph *digraph, QList<int> *node) : DirectedDFS( digraph, node )
 {
     reachable_states = new QList<int>;
+    created_nodes = new QVector<bool>(digraph->get_nodes_list()->length());
 }
 
 void DirectedDFS_DFA::search(int node)
@@ -24,6 +27,7 @@ void DirectedDFS_DFA::search(int node)
     if((*nfa)[node].connections.length() == 0)
     {
         reachable_states->append(node);
+        created_nodes->insert(node, true);
         return;
     }
 
@@ -47,4 +51,9 @@ void DirectedDFS_DFA::clear()
 {
     DirectedDFS::clear();
     reachable_states->clear();
+}
+
+QVector<bool>* DirectedDFS_DFA::get_created_nodes() const
+{
+    return created_nodes;
 }
--- directeddfs_dfa.h
+++ directeddfs_dfa.h
@@ -1,11 +1,13 @@
 #ifndef DIRECTEDDFS_DFA_H
 #define DIRECTEDDFS_DFA_H
 #include "directeddfs.h"
+#include <QVector>
 
 class DirectedDFS_DFA : DirectedDFS
 {
 private:
     QList<int>* reachable_states;
+    QVector<bool>* created_nodes;
 
 public:
     DirectedDFS_DFA(digraph::DirectedGraph *digraph);
@@ -17,6 +19,7 @@ public:
     QList<int>* get_reachable_states() const;
     // clear 'marked' array, and 'reachable_states' array
     void clear();
+    QVector<bool>* get_created_nodes() const;
 };
 
 #endif // DIRECTEDDFS_DFA_H
