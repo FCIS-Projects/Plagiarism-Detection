dc5530301f33f1a208992be6b9e4ea0641580fd6
Baron Leonardo <mohamedayman.fcis@zoho.com>
2015-12-20 19:29:45 +0200

    Create Ternary Search Trie

---
+++ tst.cpp
@@ -0,0 +1,18 @@
+#include "tst.h"
+
+using namespace tries;
+
+TST::TST()
+{
+}
+
+//int TST::get(QString key)
+//{
+//    Node *node = get(root, key, 0);
+//}
+
+//Node* TST::insert(Node* node, QString key, int value, int char_index)
+//{
+//    if     ( node == NULL ) return NULL;
+////    else if( char_index )
+//}
---
+++ tst.h
@@ -0,0 +1,37 @@
+#ifndef TST_H
+#define TST_H
+
+#include <QString>
+#include <QList>
+
+namespace tries
+{
+    struct Node
+    {
+        int value;
+        char character;
+
+        Node* left;
+        Node* middle;
+        Node* right;
+    };
+
+    class TST
+    {
+    private:
+        Node* root;
+
+    public:
+        TST();
+        ~TST();
+
+        int get(QString key);
+        void insert(QString key, int value);
+        Node* insert(Node* node, QString key, int value, int char_index);
+
+    private:
+        Node* get(Node* node, QString key, int char_index);
+    };
+}
+
+#endif // TST_H
