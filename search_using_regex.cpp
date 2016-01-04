60343341dec494c38754778832a99d8970fcc023
Baron Leonardo <mohamedayman.fcis@zoho.com>
2016-01-04 13:13:53 +0200

    create a 'prepare' method for tokenizing the code into code and comments

--- search_using_regex.cpp
+++ search_using_regex.cpp
@@ -1,6 +1,44 @@
 #include "search_using_regex.h"
+#include <QDebug>
+#include <QRegularExpression>
 
 Search_Using_RegEx::Search_Using_RegEx()
 {
 
+}
+
+Search_Using_RegEx::Search_Using_RegEx( QString *regular_expression, QString *str )
+{
+
+}
+
+Search_Using_RegEx::Search_Using_RegEx( QString *regular_expression, QFile *file )
+{
+    this->file = file;
+    this->regular_expression = regular_expression;
+    prepare();
+}
+
+void Search_Using_RegEx::prepare()
+{
+//    QFile file("in.txt");
+//    if (!file->open(QIODevice::ReadOnly | QIODevice::Text))
+//        return;
+    file->open(QIODevice::ReadOnly | QIODevice::Text);
+    QTextStream in( file );
+    QString line;
+    int comment_index;
+    QRegularExpression *regex = new QRegularExpression("\\|/*");
+
+    while ( !in.atEnd() )
+    {
+        line = in.readLine();
+
+
+
+        if( comment_index = line.indexOf("\\") -1 )
+        {
+
+        }
+    }
 }
--- search_using_regex.h
+++ search_using_regex.h
@@ -1,13 +1,35 @@
 #ifndef SEARCH_USING_REGEX_H
 #define SEARCH_USING_REGEX_H
 
+#include <QString>
+#include <QFile>
+#include <QVector>
+#include <QString>
+#include <QTextStream>
+#include <QChar>
 
 class Search_Using_RegEx
 {
+private:
+    QVector<QString>* comments;
+    QVector<QString>* reset;
 
+    QString *regular_expression;
+    QFile *file;
+    QString *_string;
+    // take a file has c++ code
+    // build the regular expressions machine
+    //
 public:
+    // TODO: RegEx for ( for, while, functions, ... )
     Search_Using_RegEx();
-    void search();
+    Search_Using_RegEx( QString *regular_expression, QString *str );
+    Search_Using_RegEx( QString *regular_expression, QFile *file );
+    void search( QString *regular_expression, QString *str );
+    void search( QString *regular_expression, QFile *file );
+
+private:
+    void prepare();
 };
 
 #endif // SEARCH_USING_REGEX_H
